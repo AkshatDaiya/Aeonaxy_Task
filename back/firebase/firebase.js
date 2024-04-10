80feeb5fba21111f1d82f039ccd40de35913492c
Akshat <daiyaakshat4@gmail.com>
2024-04-10 17:53:17 +0530

    add in env

--- back/firebase/firebase.js
+++ back/firebase/firebase.js
@@ -1,12 +1,12 @@
 // firebase.js
 module.exports = {
     firebaseConfig: {
-        apiKey: "AIzaSyAmIHrbbq3AQqGgfoMoIS4QIUfjE7wa96o",
-        authDomain: "img-upload-b0cdd.firebaseapp.com",
-        projectId: "img-upload-b0cdd",
-        storageBucket: "img-upload-b0cdd.appspot.com",
-        messagingSenderId: "89422131770",
-        appId: "1:89422131770:web:829c9417c074f48fce3b03",
-        measurementId: "G-B8ZBS384FL"
+        apiKey: process.env.APIKEY,
+        authDomain: process.env.AUTHDOMAIN,
+        projectId: process.env.PROJECTID,
+        storageBucket: process.env.STORAGEBUCKET,
+        messagingSenderId: process.env.MESSAGINGSENDERID,
+        appId: process.env.APPID,
+        measurementId: process.env.MEASUREMENTId
     }
 };
